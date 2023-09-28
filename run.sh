9b6711f77db6b0817c3a1c8fcaea339d4e4f2ce8
Alan Williams <alan.williams@vercel.com>
2023-09-28 14:26:26 +0100

    alternative ignore script

--- run.sh
+++ run.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
-if [[ $VERCEL_GIT_PREVIOUS_SHA == 80f78bed0f8211600a7d5660a9c4cfa839ab984e ]]; then
-    echo "we did not cancel the deployment"
-    echo "because SHA 80f78bed0f8211600a7d5660a9c4cfa839ab984e is equal to" $VERCEL_GIT_PREVIOUS_SHA
+# if [[ $VERCEL_GIT_PREVIOUS_SHA == 80f78bed0f8211600a7d5660a9c4cfa839ab984e ]]; then
+#     echo "we did not cancel the deployment"
+#     echo "because SHA 80f78bed0f8211600a7d5660a9c4cfa839ab984e is equal to" $VERCEL_GIT_PREVIOUS_SHA
+#     exit 1
+# else
+#     echo "we have cancelled the deployment"
+#     echo "because SHA 80f78bed0f8211600a7d5660a9c4cfa839ab984e is not equal to" $VERCEL_GIT_PREVIOUS_SHA
+#     exit 0
+# fi
+
+regex_pattern="Merge branch (.+)"
+if [[ $VERCEL_GIT_COMMIT_MESSAGE =~ $regex_pattern ]]; then
+    matched_text="${BASH_REMATCH[1]}"
+    echo "Commit message matches the pattern 'Merge branch $matched_text'"
     exit 1
 else
     echo "we have cancelled the deployment"
-    echo "because SHA 80f78bed0f8211600a7d5660a9c4cfa839ab984e is not equal to" $VERCEL_GIT_PREVIOUS_SHA
     exit 0
 fi
